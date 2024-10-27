c1c219609576ae962c525676a2dae03a75ce2472
Khushi <143668604+khushxxii@users.noreply.github.com>
2024-10-27 20:54:33 +1100

    Initial Commit(actual)

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react"
 export default function Header() {
     return (
         <header>
-        <h1>My React App</h1>
+            <h1>My React App</h1>
         </header>
     )
     }
